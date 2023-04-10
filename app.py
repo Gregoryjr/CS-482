854d0defd4dae1ac928b6e964563fc5f162e4058
Gregoryjr <85505969+Gregoryjr@users.noreply.github.com>
2023-04-10 16:12:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 from transformers import pipeline 
 st.title("Milestone #2")
 text = st.text_input("write a statement")
-classifier = pipeline("zero-shot-classification")
-res = classifier(text, cata = ["offensive"],)
-print(res)
-st.button("submit")
+con = st.button("submit")
+if con:
+  classifier = pipeline("zero-shot-classification")
+  res = classifier(text, cata = ["offensive"],)
+  print(res)
+  st.button("submit")
 
