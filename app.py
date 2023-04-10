a3636941ccc26c26e335871be43d085f8c207476
Gregoryjr <85505969+Gregoryjr@users.noreply.github.com>
2023-04-10 16:09:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ import streamlit as st
 from transformers import pipeline 
 st.title("Milestone #2")
 model_name = st.text_input("which model do you want to use?")
+classifier = pipeline("zero-shot-classification")
+res = classifier("possible lables", cata = [offensive],)
+print(res)
 st.button("submit")
 
