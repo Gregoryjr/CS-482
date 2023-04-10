b3d75462417797fb9aca375cb0f1b96089b0b1c0
Gregoryjr <85505969+Gregoryjr@users.noreply.github.com>
2023-04-10 18:06:22 -0400

    Update app.py
    model is customizable

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 from transformers import pipeline 
 st.title("Milestone #2")
 text = st.text_input("write a statement")
+model = st.text_input("pick a model to check the the above statement is offensive")
 con = st.button("submit")
 if con:
-  classifier = pipeline("zero-shot-classification")
+  classifier = pipeline(model)
   res = classifier(text, candidate_labels= ["offensive"],)
   print(res)
   st.write(res)
