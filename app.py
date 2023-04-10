12f097383ab29a4b73c8aef2a02285a6f6b997e2
Gregoryjr <85505969+Gregoryjr@users.noreply.github.com>
2023-04-10 16:09:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 st.title("Milestone #2")
 model_name = st.text_input("which model do you want to use?")
 classifier = pipeline("zero-shot-classification")
-res = classifier("possible lables", cata = [offensive],)
+res = classifier("possible lables", cata = ["offensive"],)
 print(res)
 st.button("submit")
 
