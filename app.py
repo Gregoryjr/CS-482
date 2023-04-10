d6fbb90c64520b2b5252366ab3d126ec8e4f9e5e
Gregoryjr <85505969+Gregoryjr@users.noreply.github.com>
2023-04-10 16:18:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ text = st.text_input("write a statement")
 con = st.button("submit")
 if con:
   classifier = pipeline("zero-shot-classification")
-  res = classifier(text, cata = ["offensive"],)
+  res = classifier(text, candidate_labels= ["offensive"],)
   print(res)
   st.write(res)
 
