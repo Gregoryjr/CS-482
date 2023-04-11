0934d02ab1d264a96c07e85abede7397b3ebb149
Gregoryjr <85505969+Gregoryjr@users.noreply.github.com>
2023-04-11 05:02:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
 import streamlit as st
 from transformers import pipeline 
+
 st.title("Milestone #2 v2")
 text = st.text_input("write a statement")
-import streamlit as st
 
 options = ["zero-shot-classification", "cardiffnlp/twitter-roberta-base-offensive", "nill3"]
 model = st.selectbox("Select an option", options)
-
-##model = st.write("You selected:", selected_option)
 
 con = st.button("submit")
 if con:
   if model == "zero-shot-classification":
     classifier = pipeline(model)
-    res = classifier(text, candidate_labels= ["offensive"],)
-    print(res)
+    res = classifier(text, candidate_labels=["offensive"])
     st.write(res)
   
   if model == "cardiffnlp/twitter-roberta-base-offensive":
-    
-
     classifier = pipeline('text-classification', model='cardiffnlp/twitter-roberta-base-offensive', tokenizer='cardiffnlp/twitter-roberta-base-offensive')
-
-
     result = classifier(text)
-
-    
     st.write(f"Score: {result[0]['score']*100:.2f}% confidence")
-   
-
-
